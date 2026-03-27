9870395ddeeadbf6225c81f65ceab800b3276889
Sara Gallego <saragallego207@gmail.com>
2026-03-27 15:34:20 -0500

    Primer commit - app tareas

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tareasApp',
+  webDir: 'www'
+};
+
+export default config;
